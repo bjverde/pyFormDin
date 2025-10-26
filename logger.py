f6067bacc692b046178bec97e0d3ebde466ebff3
Bjverde <bjverde@yahoo.com.br>
2025-10-26 17:22:25 -0300

    :hammer: #1 melhorando para vários tipos de log

--- logger.py
+++ logger.py
@@ -6,15 +6,30 @@ import traceback
 
 class LogHelper:
     def __init__(self, log_dir, logger_prefix=None):
-        log_file = os.path.join(log_dir, "error.log")
         prefix = logger_prefix if logger_prefix is not None else "pyFormDin_"
         self.logger = logging.getLogger(f"{prefix}{id(self)}")
-        self.logger.setLevel(logging.ERROR)
-        if not self.logger.hasHandlers():
-            handler = TimedRotatingFileHandler(log_file, when="midnight", backupCount=30, encoding="utf-8")
-            formatter = logging.Formatter('[%(asctime)s] %(levelname)s %(message)s')
-            handler.setFormatter(formatter)
-            self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Handler para erros
+        error_file = os.path.join(log_dir, "error.log")
+        error_handler = TimedRotatingFileHandler(error_file, when="midnight", backupCount=30, encoding="utf-8")
+        error_handler.setLevel(logging.ERROR)
+        error_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        self.logger.addHandler(error_handler)
+
+        # Handler para warnings
+        warning_file = os.path.join(log_dir, "warning.log")
+        warning_handler = TimedRotatingFileHandler(warning_file, when="midnight", backupCount=30, encoding="utf-8")
+        warning_handler.setLevel(logging.WARNING)
+        warning_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        self.logger.addHandler(warning_handler)
+
+        # Handler para info/debug
+        info_file = os.path.join(log_dir, "info.log")
+        info_handler = TimedRotatingFileHandler(info_file, when="midnight", backupCount=30, encoding="utf-8")
+        info_handler.setLevel(logging.INFO)
+        info_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        self.logger.addHandler(info_handler)
 
     def log_info(self, msg):
         self.logger.info(msg)
