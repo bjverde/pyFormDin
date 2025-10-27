7c0c9386a16a72be6d107586cf2d8722b4319acd
Bjverde <bjverde@yahoo.com.br>
2025-10-26 22:49:24 -0300

    :hammer: #1 criando filtros de log

--- log_pyformdin.py
+++ log_pyformdin.py
@@ -2,6 +2,13 @@ import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        super().__init__()
+        self.level = level
+    def filter(self, record):
+        return record.levelno == self.level
+
 class LogPyFormDin:
     def __init__(self, log_dir, logger_prefix=None):
         prefix = logger_prefix if logger_prefix is not None else "pyFormDin_"
@@ -13,6 +20,7 @@ class LogPyFormDin:
         error_handler = TimedRotatingFileHandler(error_file, when="midnight", backupCount=30, encoding="utf-8")
         error_handler.setLevel(logging.ERROR)
         error_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        error_handler.addFilter(LevelFilter(logging.ERROR))
         self._add_unique_handler(self.logger, error_handler, error_file)
 
         # Handler para warnings
@@ -20,6 +28,7 @@ class LogPyFormDin:
         warning_handler = TimedRotatingFileHandler(warning_file, when="midnight", backupCount=30, encoding="utf-8")
         warning_handler.setLevel(logging.WARNING)
         warning_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        warning_handler.addFilter(LevelFilter(logging.WARNING))
         self._add_unique_handler(self.logger, warning_handler, warning_file)
 
         # Handler para info/debug
@@ -27,6 +36,7 @@ class LogPyFormDin:
         info_handler = TimedRotatingFileHandler(info_file, when="midnight", backupCount=30, encoding="utf-8")
         info_handler.setLevel(logging.INFO)
         info_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s %(message)s'))
+        info_handler.addFilter(LevelFilter(logging.INFO))
         self._add_unique_handler(self.logger, info_handler, info_file)
 
     def _add_unique_handler(self, logger, handler, log_file):
