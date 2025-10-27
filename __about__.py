d95e7e16fc9bf3fc9517936b8065e7bb369e92a9
Bjverde <bjverde@yahoo.com.br>
2025-10-26 21:18:55 -0300

    :hammer: criando about

---
+++ __about__.py
@@ -0,0 +1,8 @@
+# pyFormDin/__about__.py
+
+__name__ = "pyFormDin"
+__version__ = "0.1.0"
+__author__ = "Reinaldo Barreto Junior"
+__github__ = "https://github.com/bjverde/pyFormDin/"
+__license__ = "GPL v3"
+__description__ = "Mini FrameWork em Python semelhante ao FormDin do PHP"
